40185b7fa050bf6c578522b5ac2c727cd0d73cd6
laquiante <33266194+laquiante@users.noreply.github.com>
2025-04-03 07:43:30 +0200

    Create service_catalog.sh

---
+++ service_catalog.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/bash
+clear
+echo
+echo "****************** Service Katalog ***********************"
+echo "*                                                        *"
+echo "*    Demo, Hamburg 2025 - 03.04.2025                    *"
+echo "*                                                        *"
+echo "*                                                        *"
+echo "*    1  Hausputz TBD                                     *"
+echo "*    2  IPv6                                             *"
+echo "*    3  Ende                                             *"
+echo "*                                                        *"
+echo "********************** end *******************************"
+echo
+echo 
+PS3='Auswahl: '
+options=("A" "B" "Ende")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "A")
+            echo "Auswahl Hausputz"
+            sudo ansible-playbook -i /home/cumulus/HB/inventory/files/hosts /home/cumulus/HB/.cleanup/main.yaml
+            break
+            ;;
+        "B")
+            echo "Auswahl IPv6"
+            sudo ansible-playbook -i /home/cumulus/HB/inventory/files/hosts /home/cumulus/HB/.D/main.yaml
+            break
+            ;;
+         "Ende")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
